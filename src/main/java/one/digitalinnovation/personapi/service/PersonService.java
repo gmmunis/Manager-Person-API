8416fc76cea36d5347491ff5dc38c9232efbef17
guilherme.munis <guilherme.munis@compasso.com.br>
2022-03-09 17:01:16 -0300

    Add @Service Annotation

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -4,7 +4,9 @@ import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PersonService {
 
     private PersonRepository personRepository;
